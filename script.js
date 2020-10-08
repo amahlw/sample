af565a8e1efbf29b19b00051cad65d7d97d4dae8
Kash <amahlw@gmail.com>
2020-10-08 02:27:06 -0400

    new  up

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 // The headline element
 const display = document.querySelector('#headline-display')
-console.log(display)
 
 // The input for size
 const inputSize = document.querySelector('#input-size')
-console.log(inputSize)
     // The element that displays the value 
 const showSize = document.querySelector('#show-size')
-    // Repeat this for the input and "show" elements
-    // adding event listener cause thats how it functions 
+
+// adding event listener cause thats how it functions 
 inputSize.addEventListener('input', handleInput)
 const inputFont = document.querySelector('#select-font')
 const showFont = document.querySelector('#show-font')
@@ -22,22 +20,19 @@ selectedBG.addEventListener('input', handleInput)
 const inputText = document.querySelector('#enter-text')
 const showText = document.querySelector('#headline-display')
 inputText.addEventListener('input', handleInput)
-    // style properties 
+
 function handleInput() {
     // font size:
     const fontSize = inputSize.value + 'px'
-    console.log(fontSize)
         // style 
     display.style.fontSize = fontSize
 
     // size
     showSize.innerHTML = fontSize
     const selectedFont = inputFont.value
-    console.log(selectedFont)
     display.style.selectedFont = selectedFont
     showFont.innerHTML = selectedFont
     const fontColor = inputColor.value
-    console.log(fontColor)
     display.style.fontColor = fontColor
     showColor.innerHTML = fontColor
     const bgColor = selectedBG.value
